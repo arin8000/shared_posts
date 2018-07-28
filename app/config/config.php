fddb22966570a863f5a79f56dbfcf81838036ba7
arin <arin@Nairis-MacBook-Pro.local>
2018-07-28 08:35:52 -0700

    db connection

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+// DB params
+define('DB_HOST' , 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS' , 'root');
+define('DB_NAME' , 'amvc');
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://localhost:8888/arinMVC');
+// Site name
+define('SITENAME', 'ArinMVC');
---
+++ app/libraries/controller.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * Base controller
+     * Loads the model and views
+     */
+    class Controller{
+        // Load Model
+        public function model($model){
+            // Require model file
+            require_once('../app/models/' . $model . '.php');
+
+            // instantiate model
+            return new $model();
+        }
+
+        // Load view
+        public function view($view, $data = []){
+            // check for view file
+            if(file_exists('../app/views/' . $view . '.php')){
+                require_once('../app/views/' . $view . '.php');
+            } else{
+                // view does not exist
+                die('View does not exist');
+            }
+        }
+    }
